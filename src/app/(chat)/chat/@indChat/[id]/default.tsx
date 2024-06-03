6234ded7caebbe26b0ab13924c407589ed129a20
happy.patel <happy.patel@crestinfotech.co.in>
2024-06-03 12:21:58 +0530

    deployment error solved

--- src/app/(chat)/chat/@indChat/[id]/default.tsx
+++ src/app/(chat)/chat/@indChat/[id]/default.tsx
@@ -2,16 +2,40 @@
 
 import ChatHeader from "@/components/client-components/chat/ChatHeader";
 import Message from "@/components/client-components/chat/Message";
+import Input from "@/components/client-components/common-components/Input";
 import { messages, users } from "@/utils/data";
-import { TextInput } from "flowbite-react";
+import { CloseIcon } from "@/utils/svgs";
+import { useFormik } from "formik";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
 
+interface Message {
+  username: string;
+  issentbyme: Boolean;
+  message: string;
+  avatar: string;
+  reaction?: string;
+}
+
 const Chats = ({ params }: { params: { id: string } }) => {
+  const [replyMsg, setReplyMsg] = React.useState<Message | null>(null);
   const ref = React.useRef<HTMLDivElement>(null);
   const user = users.find((user) => user.id === parseInt(params.id));
   const router = useRouter();
+
+  const formik = useFormik({
+    initialValues: {
+      msg: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const handleReply = (msg: any) => {
+    setReplyMsg(msg);
+  };
 
   React.useEffect(() => {
     if (ref.current) ref.current.scrollTo(0, ref.current.scrollHeight);
@@ -24,10 +48,12 @@ const Chats = ({ params }: { params: { id: string } }) => {
         avatar={user?.avatarImage}
         router={router}
       />
+
       <div ref={ref} className="overflow-y-scroll h-[calc(100dvh-140px)]">
         <div className="bg-[#E9ECEF] dark:bg-customGrey-black text-black dark:text-white">
           {messages.map((message, index) => (
             <Message
+              handleReply={handleReply}
               message={{
                 ...message,
                 reaction: message.reaction || "",
@@ -43,18 +69,28 @@ const Chats = ({ params }: { params: { id: string } }) => {
           ))}
         </div>
       </div>
-      <div className="w-full p-4  h-[70px] absolute bottom-0 left-0 flex gap-4 shadow border-t border-gray-100 dark:border-gray-600">
-        <TextInput
-          className="w-[95%]"
-          style={{
-            border: 0,
-            fontWeight: "bold",
-            boxShadow: "none",
-            outlineColor: "#38C585",
-          }}
-          placeholder="Type here"
-        />
 
+      <div className="w-full p-4  h-[70px] absolute bottom-0 left-0 flex gap-4 shadow border-t border-gray-100 dark:border-gray-600">
+        {replyMsg && (
+          <div className="absolute top-0 -translate-y-[100%] w-full left-0 p-4 bg-white">
+            <div className="border-l-[5px] flex justify-between items-center border-primary p-3 bg-gray-100 rounded-md font-semibold">
+              <p> {replyMsg.message}</p>
+              <button className="" onClick={()=>setReplyMsg(null)}>{CloseIcon()}</button>
+            </div>
+          </div>
+        )}
+        <Input
+          label={""}
+          type={"text"}
+          required={false}
+          classes={undefined}
+          iconClass={undefined}
+          LeftIcon={undefined}
+          RightIcon={undefined}
+          placeholder="Enter your Message Here"
+          {...formik.getFieldProps("msg")}
+          error={formik.errors.msg}
+        />
         <button className="text-white flex w-[40px] justify-center items-center  rounded-full hover:outline-primary hover:outline">
           <Image src={"/svgs/send.svg"} width={30} height={30} alt="send" />
         </button>
