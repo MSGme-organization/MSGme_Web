bc08f7e1d8b2c059571b95592d8eee4eb21313fe
happy.patel <happy.patel@crestinfotech.co.in>
2024-06-03 15:52:17 +0530

    light mode forward msg

--- src/app/(chat)/chat/@indChat/[id]/page.tsx
+++ src/app/(chat)/chat/@indChat/[id]/page.tsx
@@ -4,6 +4,7 @@ import UserItem from "@/components/client-components/chat-list/UserItem";
 import ChatHeader from "@/components/client-components/chat/ChatHeader";
 import Message from "@/components/client-components/chat/Message";
 import Input from "@/components/client-components/common-components/Input";
+import ForwardModal from "@/components/client-components/modals/ForwardModal";
 import { messages, users } from "@/utils/data";
 import { CloseIcon } from "@/utils/svgs";
 import { Modal } from "flowbite-react";
@@ -12,7 +13,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
 
-interface Message {
+export interface Message {
   username: string;
   issentbyme: Boolean;
   message: string;
@@ -112,44 +113,7 @@ const Chats = ({ params }: { params: { id: string } }) => {
           </div>
         </div>
       </div>
-      <Modal
-        show={Boolean(forwardMsg)}
-        onClose={() => setForwardMsg(null)}
-        dismissible
-      >
-        <Modal.Header>
-          <h1 className="font-semibold">Forward Message</h1>
-        </Modal.Header>
-        <Modal.Body className="dark:bg-customGrey-blackBg bg-gray-400 text-black dark:text-white">
-          {forwardMsg && (
-            <div className="w-full bg-white mb-4 dark:bg-customGrey-dark text-black dark:text-white rounded-md " >
-              <div className="border-l-[5px] flex justify-between items-center border-primary p-3 rounded-md  font-semibold">
-                <p> {forwardMsg.message}</p>
-              </div>
-            </div>
-          )}
-          {users.slice(0, 5).map((user, index) => (
-            <div
-              className="w-full px-4 py-3 flex justify-between items-center"
-              key={index}
-            >
-              <div className="flex gap-5 items-center">
-                <Image
-                  src={user.avatarImage}
-                  alt={`${user.name}'s avatar`}
-                  width={40}
-                  height={40}
-                  className="rounded-full bg-green-300  aspect-square"
-                />
-                <p className="font-semibold">{user.name}</p>
-              </div>
-              <button className="bg-primary px-4 text-white font-bold rounded-full text-[18px]">
-                Send
-              </button>
-            </div>
-          ))}
-        </Modal.Body>
-      </Modal>
+      <ForwardModal forwardMsg={forwardMsg} setForwardMsg={setForwardMsg} />
     </>
   );
 };
