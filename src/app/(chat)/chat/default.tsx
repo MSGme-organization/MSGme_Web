d14fd96501f700ab5ae37e8219a70ea196c82e98
happy.patel <happy.patel@crestinfotech.co.in>
2024-06-13 13:36:48 +0530

    default page changes

--- src/app/(chat)/chat/default.tsx
+++ src/app/(chat)/chat/default.tsx
@@ -1,19 +1,20 @@
 "use client";
 
+import React from "react";
 import ChatListNavBar from "@/components/client-components/chat-list/ChatListNavBar";
 import UserItem from "@/components/client-components/chat-list/UserItem";
+
 import { users } from "@/utils/data";
-import { BarIcon, SearchIcon } from "@/utils/svgs";
+import { SearchIcon } from "@/utils/svgs";
 import { TextInput } from "flowbite-react";
 import { useParams, useRouter } from "next/navigation";
-import React from "react";
+import Input from "@/components/client-components/common-components/Input";
 
 const Chat = () => {
-  const router = useRouter();
-  const { id } = useParams();
   const [filteredList, setFilteredList] = React.useState(users);
   const [searchString, setSearchString] = React.useState("");
-  const [isOpen, setIsOpen] = React.useState(true);
+  const router = useRouter();
+  const params = useParams();
 
   const handleFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -24,30 +25,23 @@ const Chat = () => {
     setFilteredList(filtered);
   };
 
-  const handleNavigation = (path: string, state: object = {}) => {
-    router.push(path, state);
-  };
-
-  const handleOpen = () => {
-    setIsOpen(!isOpen);
-  };
+  const handleNavigation = React.useCallback(
+    (path: string, state: object = {}) => {
+      router.push(path, state);
+    },
+    []
+  );
 
   return (
     <>
-      <div
-        className={`w-full h-full overflow-y-scroll md:w-[20%] min-w-[320px] bg-white dark:bg-customGrey-black text-black dark:text-white ${
-          isOpen ? "" : "-translate-x-[100%] duration-90 transition-all hidden"
-        }`}
-      >
-        <ChatListNavBar
-          handleNavigation={handleNavigation}
-          handleOpen={handleOpen}
-        />
+      <div className="w-full h-full overflow-y-scroll md:w-[20%] min-w-[320px] bg-white dark:bg-customGrey-black text-black dark:text-white">
+        <ChatListNavBar handleNavigation={handleNavigation} />
         <div className="p-4 sticky top-0 bg-white dark:bg-customGrey-black">
-          <TextInput
+          <Input
+            type="text"
             onChange={handleFilter}
-            rightIcon={SearchIcon}
-            className="text-customGrey"
+            RightIcon={SearchIcon}
+            classes="text-customGrey"
             placeholder="Search Chat here"
           />
         </div>
@@ -56,13 +50,15 @@ const Chat = () => {
           filteredList?.map((data, index) => (
             <UserItem
               {...data}
-              isActive={data.id === Number(id)}
+              isActive={data.id === Number(params.id)}
               handleNavigation={handleNavigation}
               key={index}
             />
           ))
         ) : (
-          <p className="text-center text-gray-500 mt-4">No user found!</p>
+          <p className="text-center text-gray-500 dark:text-white mt-4">
+            No user found!
+          </p>
         )}
       </div>
     </>
