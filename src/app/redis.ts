65ef4b380c9273a755ca0405b054b65b4fc55163
happy1511 <140176910+happy1511@users.noreply.github.com>
2024-07-06 20:34:40 +0530

    max request redis

--- src/app/redis.ts
+++ src/app/redis.ts
@@ -4,6 +4,8 @@ const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
   password: process.env.REDIS_PASSWORD,
+  maxRetriesPerRequest: null,
+  username: process.env.REDIS_USERNAME,
 });
 
 export default redis;
