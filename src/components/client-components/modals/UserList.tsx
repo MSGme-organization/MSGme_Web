7e94dbabc0eb47e85b6d625a1e5c71a354cc301a
Sahil Patel <sahilp14323@gmail.com>
2024-08-15 19:13:55 +0530

    UserList component for add new users

---
+++ src/components/client-components/modals/UserList.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { CldImage } from "next-cloudinary";
+
+const UserList = ({ filteredArr }) => {
+    console.log("UserList", filteredArr)
+    return (
+        filteredArr && filteredArr.length !== 0 ? filteredArr.map((user: any) => (
+            <div
+                className="w-full py-4 flex justify-between items-center"
+                key={user?.id}
+            >
+                <div className="flex gap-5 items-center">
+                    <CldImage
+                        src={user?.avatar || ""}
+                        alt={`${user?.username}'s avatar`}
+                        width={40}
+                        height={40}
+                        loading="lazy"
+                        className="rounded-full bg-green-300  aspect-square"
+                    />
+                    <p className="font-semibold">{user?.username}</p>
+                </div>
+                <button className="bg-primary px-4 text-white font-bold rounded-full text-[15px]">
+                    +
+                </button>
+            </div>
+        )) : (<div className="w-full py-4 flex justify-center items-center">No User Found</div>)
+    )
+}
+
+export default UserList
